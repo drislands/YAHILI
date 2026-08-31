1afe2d986db1d50d6cd129f111926179f0d3805c
Islands <drislands@users.noreply.github.com>
2026-08-31 11:48:22 -0400

    Scanning: Get Tokens
    Added a skeleton `tokensFromSource` function along with a flat Token type.
    
    Also added an exit code of 64 for when called with too many arguments, to be in parity with the book.

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,8 @@ import System.Environment (getArgs)
 import System.Directory
 import qualified Data.List.NonEmpty as NE
 import Control.Monad
+import System.Exit (exitWith, ExitCode (ExitFailure))
+import Scan (tokensFromSource)
 
 main :: IO ()
 main = do
@@ -15,7 +17,9 @@ main = do
         Nothing -> runPrompt
         Just as -> case NE.length as of
             1 -> runFile (NE.head as)
-            _ -> usage 
+            _ -> do 
+                usage 
+                exitWith (ExitFailure 64)
 
 runPrompt :: IO ()
 runPrompt = do
@@ -39,4 +43,7 @@ usage :: IO ()
 usage = putStrLn "Usage: yahili [script]"
 
 run :: String -> IO ()
-run = undefined
+run source = do
+    let tokens = tokensFromSource source
+
+    mapM_ (putStrLn . show) tokens
---
+++ src/Scan.hs
@@ -0,0 +1,7 @@
+module Scan where
+
+data Token = Token deriving (Show)
+
+
+tokensFromSource :: String -> [Token]
+tokensFromSource = undefined
