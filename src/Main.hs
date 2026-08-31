b111cce96840a620bfcc036207f503eabc221aaf
Islands <drislands@users.noreply.github.com>
2026-08-31 11:40:17 -0400

    Scanning: Run File, Read Line
    Added the next bit of logic for reading a line or a file and then running on the contents. Note that the `getLine` function doesn't have nice up-arrow history stuff. Apparently the System.Console.Haskeline module from `haskeline` can do that. Worth considering for the future.

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,10 @@
 module Main (main) where
 
+import System.IO
 import System.Environment (getArgs)
+import System.Directory
 import qualified Data.List.NonEmpty as NE
+import Control.Monad
 
 main :: IO ()
 main = do
@@ -15,10 +18,25 @@ main = do
             _ -> usage 
 
 runPrompt :: IO ()
-runPrompt = undefined
+runPrompt = do
+    putStr " > "
+    hFlush stdout
+    done <- isEOF
+    unless done $ do
+        input <- getLine
+        run input
+        runPrompt
 
 runFile :: FilePath -> IO ()
-runFile = undefined
+runFile file = do
+    exists <- doesFileExist file
+    if not exists then usage else do
+        handle <- openFile file ReadMode
+        contents <- hGetContents handle
+        run contents
 
 usage :: IO ()
 usage = putStrLn "Usage: yahili [script]"
+
+run :: String -> IO ()
+run = undefined
