96ae8d0b92c6fc00416dd1a5a2827a03bc1ca036
Islands <drislands@users.noreply.github.com>
2026-08-31 12:37:04 -0400

    Scanning: Token Types
    Defined all the token types the scanner will need. Because we're not in Java, I've put `Lx_` in front of all the values so there's no chance of mixing them up with other modules (like String from Prelude).

--- src/Scan.hs
+++ src/Scan.hs
@@ -2,6 +2,24 @@ module Scan where
 
 data Token = Token deriving (Show)
 
+data TokenType =
+    -- Single character
+    Lx_LeftParen  | Lx_RightParen   | Lx_LeftBrace | Lx_RightBrace |
+    Lx_Comma      | Lx_Dot          | Lx_Minus     | Lx_Plus       |
+    Lx_Semicolon  | Lx_Slash        | Lx_Star      |
+    -- Single and double
+    Lx_Bang       | Lx_BangEqual    | Lx_Equal     | Lx_EqualEqual |
+    Lx_Greater    | Lx_GreaterEqual | Lx_Less      | Lx_LessEqual  |
+    -- Literals
+    Lx_Identifier | Lx_String       | Lx_Number    |
+    -- Keywords
+    Lx_And        | Lx_Class        | Lx_Else      | Lx_False      |
+    Lx_Fun        | Lx_For          | Lx_If        | Lx_Nil        |
+    Lx_Or         | Lx_Print        | Lx_Return    | Lx_Super      |
+    Lx_This       | Lx_True         | Lx_Var       | Lx_While      |
+    -- And EOF by itself
+    Lx_EOF
+
 
 tokensFromSource :: String -> [Token]
 tokensFromSource = undefined
