cf238691165f041b9721dab18b8a1392a9dd7c70
Islands <drislands@users.noreply.github.com>
2026-09-03 15:35:02 -0400

    Parsing: Equality
    Started the parsing logic.
    
    The `parseEquality` function here is right-associative I think -- the tokens `5`,`==`,`3`,`==`,`2` would create `5 == ( 3 == 2 )` instead of the desired `( 5 == 3 ) == 2`, if I'm not mistaken. Will have to see if I'm right, and if so, how to fix that.

--- src/Parse.hs
+++ src/Parse.hs
@@ -2,8 +2,18 @@
 
 module Parse where
 
+import Control.Monad.Writer
+import qualified Data.List.NonEmpty as NE
+
 import Language
 import Scan
+
+data ParseError =
+    ParseError deriving (Show,Eq)
+
+type Parsing a = Writer [ParseError] a
+
+type Tokens = [Token]
 
 parenthesize :: String -> [Expression] -> String
 parenthesize name es =
@@ -18,3 +28,29 @@ uglyPrint = \case
     LString  s             -> s
     LBoolean b             -> show b 
     LNumber  n             -> show n
+
+parse :: Tokens -> Parsing Expression
+parse tokens = undefined
+
+parseExpression :: Tokens -> Parsing (Expression,Tokens)
+parseExpression = parseEquality
+
+parseEquality :: Tokens -> Parsing (Expression,Tokens)
+parseEquality tokens = do
+    (left,tokens') <- parseComparison tokens
+    case empty tokens' of
+        Nothing -> pure (left,[])
+        Just (t NE.:| rest) | getTokenType t `elem` [Lx_BangEqual,Lx_EqualEqual] -> do
+            (right,tokens'') <- parseEquality rest
+            let expr = Binary left t right
+            pure (expr,tokens'')
+                            | otherwise -> pure (left,tokens')
+
+
+parseComparison = undefined
+
+empty :: Tokens -> Maybe (NE.NonEmpty Token)
+empty ts = case NE.nonEmpty ts of
+    Nothing -> Nothing
+    Just ts'@(t NE.:| _) | getTokenType t == Lx_EOF -> Nothing
+                         | otherwise      -> Just ts'
