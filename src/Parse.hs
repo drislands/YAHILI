24a682eac47f66009250972d0f9989f1098b3179
Islands <drislands@users.noreply.github.com>
2026-09-03 16:31:33 -0400

    Parsing: Other Binary Expressions
    I started implementing the `parseComparison` function and realized it's literally identical save for the next function up and the list of token types to watch for -- so I created `parseBinary` which takes that code, turns the specific parts generic (the next function and the types), and then calls it all.
    
    Now `parseEquality` and the other 3 binary expression functions all call this helper function and look much cleaner, if a bit less immediately obvious what they're doing.

--- src/Parse.hs
+++ src/Parse.hs
@@ -36,20 +36,35 @@ parseExpression :: Tokens -> Parsing (Expression,Tokens)
 parseExpression = parseEquality
 
 parseEquality :: Tokens -> Parsing (Expression,Tokens)
-parseEquality tokens = do
-    (left,tokens') <- parseComparison tokens
+parseEquality = parseBinary parseComparison [Lx_BangEqual,Lx_EqualEqual]
+
+parseComparison :: Tokens -> Parsing (Expression,Tokens)
+parseComparison = parseBinary parseTerm [Lx_Greater,Lx_GreaterEqual,Lx_Less,Lx_LessEqual]
+
+parseTerm :: Tokens -> Parsing (Expression, Tokens)
+parseTerm = parseBinary parseFactor [Lx_Minus,Lx_Plus]
+
+parseFactor :: Tokens -> Parsing (Expression, Tokens)
+parseFactor = parseBinary parseUnary [Lx_Slash,Lx_Star]
+
+parseUnary = undefined
+
+-- For Binary expressions, the form is ultimately the same: call the next function up in the
+-- precedence ladder, then possibly loop on the results depending on if this function's
+-- operator is found.
+parseBinary :: (Tokens -> Parsing (Expression,Tokens)) -> [TokenType] -> Tokens -> Parsing (Expression,Tokens)
+parseBinary nextFunc types tokens = do
+    (left,tokens') <- nextFunc tokens
     loop left tokens'
   where
     loop :: Expression -> Tokens -> Parsing (Expression,Tokens)
     loop expr ts = case empty ts of
-        Just (t NE.:| rest) | getTokenType t `elem` [Lx_BangEqual,Lx_EqualEqual] -> do
-            (right,ts') <- parseComparison rest
+        Just (t NE.:| rest) | getTokenType t `elem` types -> do
+            (right,ts') <- nextFunc rest
             loop (Binary expr t right) ts'
         _ -> pure (expr,ts)
 
-
-parseComparison = undefined
-
+-- For our purposes, hitting an EOF is the same as hitting the end of the list.
 empty :: Tokens -> Maybe (NE.NonEmpty Token)
 empty ts = case NE.nonEmpty ts of
     Nothing -> Nothing
