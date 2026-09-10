9c7293ef939508313fd6d73e7f40ecf46632c5c4
Islands <drislands@users.noreply.github.com>
2026-09-10 16:54:23 -0400

    Turing (AGY): For Loops
    Constructed the `for` loop function, with the same desugaring logic from the book, entirely on my own...except at the end I had these horrible lines:
    
    ```
        body <- parseStatement
        body' <- addIncrement mincrement body
        body'' <- addCondition mcondition body'
        addInitializer minitializer body''
    ```
    
    Surely there's a better way, I thought. But as with previous cases, I wasn't sure the best way to simplify this in Haskell.
    
    Consulted with Gemini and it helpfully pointed out that my 3 `addX` helper functions don't need to be in the Parsing monad as I had originally written them. Instead, with the `mX` variables added on, they have type `Statement -> Statement` and could be easily composed and then `<$>`'d onto the base `parseStatement` call.

--- src/Statement.hs
+++ src/Statement.hs
@@ -26,11 +26,12 @@ parseProgram'' program = do
 
 parseStatement :: Parsing Statement
 parseStatement = do
-    m <- match [Lx_Print,Lx_Var,Lx_LeftBrace,Lx_If,Lx_While]
+    m <- match [Lx_Print,Lx_Var,Lx_LeftBrace,Lx_If,Lx_While,Lx_For]
     case m of
         Just t  
             | getTokenType t == Lx_If        -> parseIfStmt
             | getTokenType t == Lx_While     -> parseWhileStmt
+            | getTokenType t == Lx_For       -> parseForStmt
             | getTokenType t == Lx_Print     -> parsePrintStmt
             | getTokenType t == Lx_Var       -> parseDeclaration
             | getTokenType t == Lx_LeftBrace -> parseBlock
@@ -56,6 +57,48 @@ parseWhileStmt = do
     consume_ Lx_RightParen "Expect ')' after condition." 
     body <- parseStatement
     pure $ WhileStatement condition body
+
+parseForStmt :: Parsing Statement
+parseForStmt = do
+    consume_ Lx_LeftParen "Expect '(' after 'for'."
+    minitializer <- parseInitializer
+    mcondition   <- parseWithoutToken Lx_Semicolon
+    consume_ Lx_Semicolon "Expect ';' after loop condition."
+    mincrement   <- parseWithoutToken Lx_RightParen
+    consume_ Lx_RightParen "Expect ')' after for clauses."
+    addInitializer minitializer
+        . addCondition mcondition
+        . addIncrement mincrement
+        <$> parseStatement
+  where
+    parseInitializer :: Parsing (Maybe Statement)
+    parseInitializer = do
+        m <- match [Lx_Semicolon,Lx_Var]
+        case m of
+            Just t
+                | getTokenType t == Lx_Semicolon -> pure Nothing
+                | getTokenType t == Lx_Var       -> Just <$> parseDeclaration
+            _                                    -> Just <$> parseStatement
+    parseWithoutToken :: TokenType -> Parsing (Maybe Expression)
+    parseWithoutToken tt = do
+        s <- peek
+        if getTokenType s == tt then pure Nothing
+        else Just <$> parseExpression
+    addIncrement :: Maybe Expression -> Statement -> Statement
+    addIncrement mex body =
+        case mex of
+            Nothing  -> body
+            Just inc -> Block [body,ExpressionStatement inc]
+    addCondition :: Maybe Expression -> Statement -> Statement
+    addCondition mex body =
+        case mex of
+            Nothing ->  WhileStatement (LBoolean True) body
+            Just con -> WhileStatement con body
+    addInitializer :: Maybe Statement -> Statement -> Statement
+    addInitializer mst body =
+        case mst of
+            Nothing -> body
+            Just st -> Block [st,body]
 
 parseDeclaration :: Parsing Statement
 parseDeclaration = do
